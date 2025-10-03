b07f3655f2afcf17c3804c1544a85bddc813ce21
B dev <bestther2021@gmail.com>
2025-10-03 12:45:17 +0700

    Update passgen.bat

--- passgen.bat
+++ passgen.bat
@@ -1,1 +1,9 @@
-passgen.bat --rich
+@echo off
+REM Check folder
+IF NOT "%CD:~-18%"=="\password-generator" (
+    echo Error: You must run this program from the 'password-generator' folder!
+    exit /b 1
+)
+
+REM Run Python script with arguments
+python password_generator.py %*
