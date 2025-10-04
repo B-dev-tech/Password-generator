e67b49790dd83df9a413d7e73c735815df522de7
B dev <bestther2021@gmail.com>
2025-10-04 10:21:50 +0700

    Update passgen.bat

--- passgen.bat
+++ passgen.bat
@@ -1,9 +1,12 @@
 @echo off
-REM Check folder
-IF NOT "%CD:~-18%"=="\password-generator" (
+REM Check folder (ignore case)
+for %%A in ("%CD%") do set FOLDER=%%~nxA
+setlocal enabledelayedexpansion
+set FOLDER=!FOLDER!
+set FOLDER=!FOLDER:~0,999!
+IF /I NOT "!FOLDER!"=="password-generator" (
     echo Error: You must run this program from the 'password-generator' folder!
     exit /b 1
 )
 
-REM Run Python script with arguments
 python password_generator.py %*
